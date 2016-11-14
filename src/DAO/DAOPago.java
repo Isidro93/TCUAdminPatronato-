baedf91f3abe6dee418db919224a47cc759461e8
mmkcesar <mmkcesar@hotmail.com>
2016-11-14 16:47:05 -0600

    creacion DAOPago

---
+++ src/DAO/DAOPago.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import Interfaces.DAOConsultas;
+
+/**
+ *
+ * @author Usuario
+ */
+public class DAOPago implements DAOConsultas{
+
+    @Override
+    public void insert(String info) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void modificar(String info) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void borrar(String info) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public String consultar(String info) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
